9a0924e7aecf310d56fa28da5c4442a1118bfcb1
Louis Maddox <louismmx@gmail.com>
2023-08-24 23:18:10 +0100

    build: bump -> v0.1.1

--- src/campbells/__init__.py
+++ src/campbells/__init__.py
@@ -16,7 +16,7 @@ For more than you ever wanted to know about Beautiful Soup, see their
 documentation: http://www.crummy.com/software/BeautifulSoup/bs4/doc/
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = ["BeautifulSoup"]
 
