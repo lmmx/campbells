eb037aae14a9e0ab67004e0241e8f8160aaf793c
Louis Maddox <louismmx@gmail.com>
2023-08-26 21:41:57 +0100

    refactor(dammit): further factor out the dammit subpackage

--- src/campbells/dammit/detection.py
+++ src/campbells/dammit/detection.py
@@ -1,40 +1,11 @@
-"""Campbells bonus library: Unicode, Dammit
-
-This library converts a bytestream to Unicode through any means
-necessary. It is heavily based on code from Mark Pilgrim's Universal
-Feed Parser. It works best on XML and HTML, but it does not rewrite the
-XML or HTML to reflect a new encoding; that's the tree builder's job.
-"""
 import codecs
 import logging
 import re
-from collections import defaultdict
-from contextlib import suppress
 
-# Use of this source code is governed by the MIT license.
-from html.entities import codepoint2name, html5
+from .dependency_resolution import chardet_module
+from .encodings import encoding_res
 
-__all__ = ["chardet_dammit", "EntitySubstitution", "EncodingDetector", "UnicodeDammit"]
-
-# Import a library to autodetect character encodings. We'll support
-# any of a number of libraries that all support the same API:
-#
-# * cchardet
-# * chardet
-# * charset-normalizer
-chardet_module = None
-try:
-    #  PyPI package: cchardet
-    import cchardet as chardet_module
-except ImportError:
-    try:
-        #  Debian package: python-chardet
-        #  PyPI package: chardet
-        import chardet as chardet_module
-    except ImportError:
-        with suppress(ImportError):
-            # PyPI package: charset-normalizer
-            import charset_normalizer as chardet_module
+__all__ = ["chardet_dammit", "UnicodeDammit", "EncodingDetector", "UnicodeDammit"]
 
 
 def chardet_dammit(s):
@@ -42,270 +13,6 @@ def chardet_dammit(s):
         return None
     else:
         return chardet_module.detect(s)["encoding"]
-
-
-# Build bytestring and Unicode versions of regular expressions for finding
-# a declared encoding inside an XML or HTML document.
-xml_encoding = "^\\s*<\\?.*encoding=['\"](.*?)['\"].*\\?>"
-html_meta = "<\\s*meta[^>]+charset\\s*=\\s*[\"']?([^>]*?)[ /;'\">]"
-encoding_res = dict()
-encoding_res[bytes] = {
-    "html": re.compile(html_meta.encode("ascii"), re.I),
-    "xml": re.compile(xml_encoding.encode("ascii"), re.I),
-}
-encoding_res[str] = {
-    "html": re.compile(html_meta, re.I),
-    "xml": re.compile(xml_encoding, re.I),
-}
-
-
-class EntitySubstitution:
-    """The ability to substitute XML or HTML entities for certain characters."""
-
-    def _populate_class_variables():
-        """Initialize variables used by this class to manage the plethora of
-        HTML5 named entities.
-
-        This function returns a 3-tuple containing two dictionaries
-        and a regular expression:
-
-        unicode_to_name - A mapping of Unicode strings like "⦨" to
-        entity names like "angmsdaa". When a single Unicode string has
-        multiple entity names, we try to choose the most commonly-used
-        name.
-
-        name_to_unicode: A mapping of entity names like "angmsdaa" to
-        Unicode strings like "⦨".
-
-        named_entity_re: A regular expression matching (almost) any
-        Unicode string that corresponds to an HTML5 named entity.
-        """
-        unicode_to_name = {}
-        name_to_unicode = {}
-
-        short_entities = set()
-        long_entities_by_first_character = defaultdict(set)
-
-        for name_with_semicolon, character in sorted(html5.items()):
-            # "It is intentional, for legacy compatibility, that many
-            # code points have multiple character reference names. For
-            # example, some appear both with and without the trailing
-            # semicolon, or with different capitalizations."
-            # - https://html.spec.whatwg.org/multipage/named-characters.html#named-character-references
-            #
-            # The parsers are in charge of handling (or not) character
-            # references with no trailing semicolon, so we remove the
-            # semicolon whenever it appears.
-            if name_with_semicolon.endswith(";"):
-                name = name_with_semicolon[:-1]
-            else:
-                name = name_with_semicolon
-
-            # When parsing HTML, we want to recognize any known named
-            # entity and convert it to a sequence of Unicode
-            # characters.
-            if name not in name_to_unicode:
-                name_to_unicode[name] = character
-
-            # When _generating_ HTML, we want to recognize special
-            # character sequences that _could_ be converted to named
-            # entities.
-            unicode_to_name[character] = name
-
-            # We also need to build a regular expression that lets us
-            # _find_ those characters in output strings so we can
-            # replace them.
-            #
-            # This is tricky, for two reasons.
-
-            if len(character) == 1 and ord(character) < 128 and character not in "<>&":
-                # First, it would be annoying to turn single ASCII
-                # characters like | into named entities like
-                # &verbar;. The exceptions are <>&, which we _must_
-                # turn into named entities to produce valid HTML.
-                continue
-
-            if len(character) > 1 and all(ord(x) < 128 for x in character):
-                # We also do not want to turn _combinations_ of ASCII
-                # characters like 'fj' into named entities like '&fjlig;',
-                # though that's more debateable.
-                continue
-
-            # Second, some named entities have a Unicode value that's
-            # a subset of the Unicode value for some _other_ named
-            # entity.  As an example, \u2267' is &GreaterFullEqual;,
-            # but '\u2267\u0338' is &NotGreaterFullEqual;. Our regular
-            # expression needs to match the first two characters of
-            # "\u2267\u0338foo", but only the first character of
-            # "\u2267foo".
-            #
-            # In this step, we build two sets of characters that
-            # _eventually_ need to go into the regular expression. But
-            # we won't know exactly what the regular expression needs
-            # to look like until we've gone through the entire list of
-            # named entities.
-            if len(character) == 1:
-                short_entities.add(character)
-            else:
-                long_entities_by_first_character[character[0]].add(character)
-
-        # Now that we've been through the entire list of entities, we
-        # can create a regular expression that matches any of them.
-        particles = set()
-        for short in short_entities:
-            long_versions = long_entities_by_first_character[short]
-            if not long_versions:
-                particles.add(short)
-            else:
-                ignore = "".join([x[1] for x in long_versions])
-                # This finds, e.g. \u2267 but only if it is _not_
-                # followed by \u0338.
-                particles.add(f"{short}(?![{ignore}])")
-
-        for long_entities in list(long_entities_by_first_character.values()):
-            for long_entity in long_entities:
-                particles.add(long_entity)
-
-        re_definition = "(%s)" % "|".join(particles)
-
-        # If an entity shows up in both html5 and codepoint2name, it's
-        # likely that HTML5 gives it several different names, such as
-        # 'rsquo' and 'rsquor'. When converting Unicode characters to
-        # named entities, the codepoint2name name should take
-        # precedence where possible, since that's the more easily
-        # recognizable one.
-        for codepoint, name in list(codepoint2name.items()):
-            character = chr(codepoint)
-            unicode_to_name[character] = name
-
-        return unicode_to_name, name_to_unicode, re.compile(re_definition)
-
-    (
-        CHARACTER_TO_HTML_ENTITY,
-        HTML_ENTITY_TO_CHARACTER,
-        CHARACTER_TO_HTML_ENTITY_RE,
-    ) = _populate_class_variables()
-
-    CHARACTER_TO_XML_ENTITY = {
-        "'": "apos",
-        '"': "quot",
-        "&": "amp",
-        "<": "lt",
-        ">": "gt",
-    }
-
-    BARE_AMPERSAND_OR_BRACKET = re.compile(
-        "([<>]|" "&(?!#\\d+;|#x[0-9a-fA-F]+;|\\w+;)" ")",
-    )
-
-    AMPERSAND_OR_BRACKET = re.compile("([<>&])")
-
-    @classmethod
-    def _substitute_html_entity(cls, matchobj):
-        """Used with a regular expression to substitute the
-        appropriate HTML entity for a special character string."""
-        entity = cls.CHARACTER_TO_HTML_ENTITY.get(matchobj.group(0))
-        return "&%s;" % entity
-
-    @classmethod
-    def _substitute_xml_entity(cls, matchobj):
-        """Used with a regular expression to substitute the
-        appropriate XML entity for a special character string."""
-        entity = cls.CHARACTER_TO_XML_ENTITY[matchobj.group(0)]
-        return "&%s;" % entity
-
-    @classmethod
-    def quoted_attribute_value(self, value):
-        """Make a value into a quoted XML attribute, possibly escaping it.
-
-        Most strings will be quoted using double quotes.
-
-         Bob's Bar -> "Bob's Bar"
-
-        If a string contains double quotes, it will be quoted using
-        single quotes.
-
-         Welcome to "my bar" -> 'Welcome to "my bar"'
-
-        If a string contains both single and double quotes, the
-        double quotes will be escaped, and the string will be quoted
-        using double quotes.
-
-         Welcome to "Bob's Bar" -> "Welcome to &quot;Bob's bar&quot;
-        """
-        quote_with = '"'
-        if '"' in value:
-            if "'" in value:
-                # The string contains both single and double
-                # quotes.  Turn the double quotes into
-                # entities. We quote the double quotes rather than
-                # the single quotes because the entity name is
-                # "&quot;" whether this is HTML or XML.  If we
-                # quoted the single quotes, we'd have to decide
-                # between &apos; and &squot;.
-                replace_with = "&quot;"
-                value = value.replace('"', replace_with)
-            else:
-                # There are double quotes but no single quotes.
-                # We can use single quotes to quote the attribute.
-                quote_with = "'"
-        return quote_with + value + quote_with
-
-    @classmethod
-    def substitute_xml(cls, value, make_quoted_attribute=False):
-        """Substitute XML entities for special XML characters.
-
-        :param value: A string to be substituted. The less-than sign
-          will become &lt;, the greater-than sign will become &gt;,
-          and any ampersands will become &amp;. If you want ampersands
-          that appear to be part of an entity definition to be left
-          alone, use substitute_xml_containing_entities() instead.
-
-        :param make_quoted_attribute: If True, then the string will be
-         quoted, as befits an attribute value.
-        """
-        # Escape angle brackets and ampersands.
-        value = cls.AMPERSAND_OR_BRACKET.sub(cls._substitute_xml_entity, value)
-
-        if make_quoted_attribute:
-            value = cls.quoted_attribute_value(value)
-        return value
-
-    @classmethod
-    def substitute_xml_containing_entities(cls, value, make_quoted_attribute=False):
-        """Substitute XML entities for special XML characters.
-
-        :param value: A string to be substituted. The less-than sign will
-          become &lt;, the greater-than sign will become &gt;, and any
-          ampersands that are not part of an entity defition will
-          become &amp;.
-
-        :param make_quoted_attribute: If True, then the string will be
-         quoted, as befits an attribute value.
-        """
-        # Escape angle brackets, and ampersands that aren't part of
-        # entities.
-        value = cls.BARE_AMPERSAND_OR_BRACKET.sub(cls._substitute_xml_entity, value)
-
-        if make_quoted_attribute:
-            value = cls.quoted_attribute_value(value)
-        return value
-
-    @classmethod
-    def substitute_html(cls, s):
-        """Replace certain Unicode characters with named HTML entities.
-
-        This differs from data.encode(encoding, 'xmlcharrefreplace')
-        in that the goal is to make the result more readable (to those
-        with ASCII displays) rather than to recover from
-        errors. There's absolutely nothing wrong with a UTF-8 string
-        containg a LATIN SMALL LETTER E WITH ACUTE, but replacing that
-        character with "&eacute;" will make it more readable to some
-        people.
-
-        :param s: A Unicode string.
-        """
-        return cls.CHARACTER_TO_HTML_ENTITY_RE.sub(cls._substitute_html_entity, s)
 
 
 class EncodingDetector:
@@ -540,11 +247,7 @@ class UnicodeDammit:
     # by the heuristics in find_codec.
     CHARSET_ALIASES = {"macintosh": "mac-roman", "x-sjis": "shift-jis"}
 
-    ENCODINGS_WITH_SMART_QUOTES = [
-        "windows-1252",
-        "iso-8859-1",
-        "iso-8859-2",
-    ]
+    ENCODINGS_WITH_SMART_QUOTES = ["windows-1252", "iso-8859-1", "iso-8859-2"]
 
     def __init__(
         self,
@@ -682,16 +385,12 @@ class UnicodeDammit:
             markup = smart_quotes_compiled.sub(self._sub_ms_char, markup)
 
         try:
-            # print("Trying to convert document to %s (errors=%s)" % (
-            #    proposed, errors))
-            u = self._to_unicode(markup, proposed, errors)
-            self.markup = u
+            # print(f"Trying to convert document to {proposed} ({errors=})")
+            self.markup = self._to_unicode(markup, proposed, errors)
             self.original_encoding = proposed
         except Exception:
-            # print("That didn't work!")
-            # print(e)
             return None
-        # print("Correct encoding: %s" % proposed)
+        # print(f"Correct encoding: {proposed}")
         return self.markup
 
     def _to_unicode(self, data, encoding, errors="strict"):
@@ -909,11 +608,10 @@ class UnicodeDammit:
         b"\xff": "y",
     }
 
-    # A map used when removing rogue Windows-1252/ISO-8859-1
-    # characters in otherwise UTF-8 documents.
+    # A map used when removing rogue Windows-1252/ISO-8859-1 characters in otherwise
+    # UTF-8 documents.
     #
-    # Note that \x81, \x8d, \x8f, \x90, and \x9d are undefined in
-    # Windows-1252.
+    # Note that \x81, \x8d, \x8f, \x90, and \x9d are undefined in Windows-1252.
     WINDOWS_1252_TO_UTF8 = {
         0x80: b"\xe2\x82\xac",  # €
         0x82: b"\xe2\x80\x9a",  # ‚
